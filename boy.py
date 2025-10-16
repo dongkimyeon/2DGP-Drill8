685400e4acae2f7217f40519ca295d49229291c2
dongkimyeon <114465940+dongkimyeon@users.noreply.github.com>
2025-10-16 23:57:01 +0900

    Auto런 상태에서 5초 경과 후 다시 IDLE로 돌아오는거 확인

--- boy.py
+++ boy.py
@@ -24,7 +24,7 @@ def time_out(event):
 class AutoRun:
     def __init__(self, boy):
         self.boy = boy
-
+        self.boy.running_start_time = get_time()
     def enter(self, event):
         print('AutoRun')
 
@@ -34,6 +34,8 @@ class AutoRun:
     def do(self):
         self.boy.frame = (self.boy.frame + 1) % 8
         self.boy.x += self.boy.dir * 5
+        if get_time() - self.boy.running_start_time > 5.0:
+            self.boy.state_machine.handle_state_event(('TIME_OUT', 0))
 
     def draw(self):
         if self.boy.face_dir == 1:  # right
@@ -126,7 +128,7 @@ class Boy:
             self.SLEEP : {space_down : self.IDLE},
             self.IDLE : {right_down : self.RUN, left_down : self.RUN,  time_out : self.SLEEP , a_down : self.AUTO_RUN},
             self.RUN : {right_down : self.IDLE, left_down : self.IDLE, right_up : self.IDLE , left_up : self.IDLE },
-            self.AUTO_RUN : {}
+            self.AUTO_RUN : {time_out : self.IDLE}
         })
 
     def update(self):
